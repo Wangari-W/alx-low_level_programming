fc404104ee7cf7a9dbc5395eaba8e7314cba7b24
Wangari-W <wangawachira+github@gmail.com>
2022-10-18 13:22:58 -0700

    Using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
-	*main - Entry point
-	*Description: prints "\"Programming is like building a multilingual puzzle"
+	*main - puts("\"Programming is like building a multilingual puzzle");
 	*return: 0
 	*/
 
 init main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
