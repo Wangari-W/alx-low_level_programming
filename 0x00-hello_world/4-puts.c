2ae550e632a474eadb572405bb1397abe89514fa
Wangari-W <wangawachira+github@gmail.com>
2022-10-18 12:55:21 -0700

    Using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+	*main - Entry point
+	*Description: prints "\"Programming is like building a multilingual puzzle"
+	*return: 0
+	*/
+
+init main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"
+	return (0);
+}
