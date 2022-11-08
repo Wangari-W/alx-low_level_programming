22b65656d5f00b04104e118e15d5dc0640c40f8d
Wangari-W <wangawachira+github@gmail.com>
2022-11-08 02:56:45 -0800

    Prints a quote to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - Entry point
+ *description: prints "and that piece of art is useful"
+	- Dore Korpar, 2015-10-19\n" to standard error.
+ *Return: Always 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
