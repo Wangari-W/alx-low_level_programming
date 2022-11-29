5a2c55f7dc124d662a427cf87038d29defc080cc
Wangari-W <wangawachira+github@gmail.com>
2022-11-29 11:10:36 -0800

    Lowercase Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *print hexadecimal in lowercase using putchar()
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = '0';
+	int i = 'a';
+	
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	while (i <= 'f')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
